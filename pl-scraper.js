8b53ccab8d015c48271bdd8d85b3d27b9d3b6627
wabens <waltkbenson@gmail.com>
2019-06-12 15:12:02 -0500

    full functional web scraper geting premier league info

---
+++ pl-scraper.js
@@ -0,0 +1,36 @@
+// pl-scraper.js
+
+const axios = require('axios');
+
+// allows html to be parsed with Jquery
+const cheerio = require('cheerio');
+
+const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
+
+// Get the stats of the top players of the premier league
+axios(url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html)
+
+        // targets the body of the table holding player information
+        const statsTable = $('.statsTableContainer > tr');
+        const topPremierLeagueScorers = [];
+
+        statsTable.each(function () {
+            const rank = $(this).find('.rank > strong').text();
+            const playerName = $(this).find('.playerName > strong').text();
+            const nationality = $(this).find('.playerCountry').text();
+            const goals = $(this).find('.mainStat').text();
+
+            topPremierLeagueScorers.push({
+                rank,
+                name: playerName,
+                nationality,
+                goals,
+            });
+        });
+
+        console.log(topPremierLeagueScorers);
+    })
+    .catch(console.error);
